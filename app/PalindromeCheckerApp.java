f5ca4a3cc755ee5480893b4947fe3473b1b9af77
QuantumObscura <tusharnbiswas1012@gmail.com>
2026-02-25 05:08:20 +0000

    UC2-HardCodePalidrome

--- app/PalindromeCheckerApp.java
+++ app/PalindromeCheckerApp.java
@@ -1,8 +1,20 @@
 public class PalindromeCheckerApp {
-    public static void main (string [] arg){
-        System.out.println("welcome to palindrome checker app")
-        System.out.println("use case 2")
+    public static void main(String[] args) { // Fixed String case
+        String str = "madam"; // Added semicolon
+        boolean isPalindrome = true;
 
+        for (int i = 0; i < str.length() / 2; i++) {
+            // Fixed dot notation and spelling of length()
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
+        if (isPalindrome) { // Fixed variable case
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
     }
 }
