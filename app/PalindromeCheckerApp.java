1e5f1158c17c5918a8d63d9e27c6777e680a5811
QuantumObscura <tusharnbiswas1012@gmail.com>
2026-02-25 04:23:39 +0000

    “UC2-HardCodePalidrome”

--- app/PalindromeCheckerApp.java
+++ app/PalindromeCheckerApp.java
@@ -1,5 +1,8 @@
 public class PalindromeCheckerApp {
     public static void main (string [] arg){
         System.out.println("welcome to palindrome checker app")
+        System.out.println("use case 2")
+
+
     }
 }
